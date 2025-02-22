49940e9fb1043aba9362fc60fdf56d13f2001c46
Sudhanva <sudhanva.acharya711@gmail.com>
2025-02-22 11:35:23 -0800

    Create twoSum.java

---
+++ Arrays/1. Two Sum/twoSum.java
@@ -0,0 +1,41 @@
+// HASHMAP
+
+// TWO PASS
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (map.containsKey(diff) && map.get(diff) != i) {
+                return new int[] {i, map.get(diff)};
+            }
+        }
+        return new int[0];
+    }
+}
+
+// ONE PASS
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+
+            if (map.containsKey(diff)) {
+                return new int[] {i, map.get(diff)};
+            }
+            map.put(nums[i], i);
+        }
+
+        return new int[0]; 
+        
+    }
+}
