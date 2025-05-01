653b23161d0e6c2a4eb3702d34e6df8da326edfb
Sudhanva <sudhanva.acharya711@gmail.com>
2025-05-01 15:56:19 -0700

    Create btZigzagLevelOrderTraversal.java

---
+++ Trees/103. Binary Tree Zigzag Level Order Traversal/btZigzagLevelOrderTraversal.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        while (!q.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            for (int i = q.size(); i > 0; i--) {
+                TreeNode node = q.poll();
+                level.add(node.val);
+                if (node.left != null) q.offer(node.left);
+                if (node.right != null) q.offer(node.right);
+            }
+            if (res.size() % 2 != 0) Collections.reverse(level);
+            res.add(level);
+        }
+        return res;
+    }
+}
