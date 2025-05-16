ba721ce09adbeb01f85b03961eb2ae50e60b3626
Sudhanva <sudhanva.acharya711@gmail.com>
2025-05-15 17:34:15 -0700

    Update concatenationOfArray.java

--- Arrays/1929. Concatenation of Array/concatenationOfArray.java
+++ Arrays/1929. Concatenation of Array/concatenationOfArray.java
@@ -1,3 +1,4 @@
+// SINGLE PASS
 class Solution {
     public int[] getConcatenation(int[] nums) {
         int n = nums.length;
@@ -11,3 +12,22 @@ class Solution {
         return ans;
     }
 }
+
+// TWO PASS
+
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[2 * n];
+        int idx = 0;
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < n; j++) {
+                ans[idx++] = nums[j];
+            }
+        }
+        
+        return ans;
+    }
+}
+
