de8e48cdd00d3860a423016d12d74e3c19cad7e1
Sudhanva <sudhanva.acharya711@gmail.com>
2025-06-04 22:14:23 -0700

    Create palindromeLinkedList.java

---
+++ Linked Lists/234. Palindrome Linked List/palindromeLinkedList.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+
+        if (head == null) return true;
+        Stack<Integer> stack = new Stack<>();
+
+        ListNode current = head;
+        while (current != null) {
+            stack.push(current.val);
+            current = current.next;
+        }
+
+        current = head;
+
+        while (current != null) {
+            if (stack.pop() != current.val) return false;
+            current = current.next;
+        }
+        return true;
+    }
+}
