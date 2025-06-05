183bd02d48cd5170cc7787696a4cca36cb98cd33
Sudhanva <sudhanva.acharya711@gmail.com>
2025-06-04 23:22:28 -0700

    Update palindromeLinkedList.java

--- Linked Lists/234. Palindrome Linked List/palindromeLinkedList.java
+++ Linked Lists/234. Palindrome Linked List/palindromeLinkedList.java
@@ -8,6 +8,9 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+// STACK
+
 class Solution {
     public boolean isPalindrome(ListNode head) {
 
@@ -26,6 +29,45 @@ class Solution {
             if (stack.pop() != current.val) return false;
             current = current.next;
         }
+        return true;
+    }
+}
+
+// TWO POINTERS
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+
+        if (head == null) return true;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode prev = slow;
+        ListNode current = slow.next;
+        slow.next = null;
+
+        while (current != null) {
+            ListNode temp = current.next;
+            current.next = prev;
+            prev = current;
+            current = temp;
+        }
+
+        slow = head;
+        
+        while (prev != null) {
+            if (slow.val != prev.val) {
+                return false;
+            } 
+            slow = slow.next;
+            prev = prev.next;
+        }
+
         return true;
     }
 }
